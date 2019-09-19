3ba5499d80fab690e487fe10deb1c481e8511b7d
aazocar <abril.azocar@gmail.com>
2019-09-19 18:10:22 +0200

    Add new template

--- pychromatic/colors.py
+++ pychromatic/colors.py
@@ -1,20 +1,10 @@
 
+red = ''
 
-class default:
+
+class set2:
     def __init__(self):
-        self.red = '#EF5350'
-        self.pink = '#EC407A'
-        self.purple = '#AB47BC'
-        self.deep_purple = '#7E57C2'
-        self.indigo = '#5C6BC0'
-        self.blue = '#42A5F5'
-        self.light_blue = '#29B6F6'
-        self.cyan = '#26C6DA'
-        self.teal = '#26A69A'
-        self.green = '#66BB6A'
-        self.light_green = '#9CCC65'
-        self.lime = '#D4E157'
-        self.yellow = '#FFEE58'
-        self.amber = '#FFCA28'
-        self.orange = '#FFA726'
-        self.deep_orange = '#FF7043'
+        self.colors = ['#ccece6', '#99d8c9']
+    
+    def get_colors(self):
+        return self.colors
