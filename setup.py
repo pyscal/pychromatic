408dc936570b9be5cde890530c1c5aceb7f9596a
srmnitc <sarathmenon@mailbox.org>
2020-03-21 19:51:36 +0100

    Bump version: 0.1.0 → 0.2.0-dev0

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/srmnitc/pychromatic',
-    version='0.1.0',
+    version='0.2.0-dev0',
     zip_safe=False,
 )
