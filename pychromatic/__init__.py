191642d459cf70f3bc1a4d7ce8dc237cc00d5096
srmnitc <sarathmenon@mailbox.org>
2021-02-10 19:24:28 +0100

    Bump version: 0.5.6 → 0.5.7

--- pychromatic/__init__.py
+++ pychromatic/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.5.6'
+__version__ = '0.5.7'
 
 from pychromatic.palette import Palette
 from pychromatic.plutils import Multiplot, BrokenAxes
--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/srmnitc/pychromatic',
-    version='0.5.6',
+    version='0.5.7',
     zip_safe=False,
 )
